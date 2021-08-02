f71c421029a1a916ecaf05a5ffef4f00e005162c
Virginie <65453751+Minolie06@users.noreply.github.com>
2021-08-02 15:51:11 +0200

    Added shuffle()

--- script/script.js
+++ script/script.js
@@ -1,3 +1,18 @@
+var pairLeft;
+
+function gameInit() {
+	pairLeft = 8;
+	shuffle();
+}
+
+function shuffle() {
+	let listPos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+	listPos = listPos.sort(() => Math.random() - 0.5);
+	for (i = 0; i < listPos.length; i++) {
+		cards[i].style.order = listPos[i];
+	}
+}
+
 //transforming the HTML collection into an array
 cards = Array.from(document.getElementsByClassName("card"));
 
