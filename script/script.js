834123d28825151366bbd3e80c3df939a0af35ec
Virginie <65453751+Minolie06@users.noreply.github.com>
2021-08-02 17:16:07 +0200

    Game v1

--- script/script.js
+++ script/script.js
@@ -1,8 +1,27 @@
 var pairLeft;
+var lockBoard;
+var firstCard, secondCard;
+
+var gameCards = document.getElementById("game-cards");
+//transforming the HTML collection into an array
+cards = Array.from(document.getElementsByClassName("card"));
 
 function gameInit() {
 	pairLeft = 8;
-	shuffle();
+	//reset timer and score here + unflip cards etc
+	unflipCards();
+	setTimeout(function() { //shuffle after animation
+			resetBoard();
+			shuffle();
+		}, 1000);
+	
+}
+
+function unflipCards() {
+	gameCards.classList.add("locked");
+	for (i = 0; i < cards.length; i++) {
+		cards[i].classList.remove("flipped");
+	}	
 }
 
 function shuffle() {
@@ -13,13 +32,50 @@ function shuffle() {
 	}
 }
 
-//transforming the HTML collection into an array
-cards = Array.from(document.getElementsByClassName("card"));
+function flipCard(card) {
+	card.classList.add("flipped");
+	if (firstCard == null) {
+		firstCard = card;
+	} else {
+		secondCard = card;
+		checkForMatch();
+	}
+}
+
+function checkForMatch(){
+	if (firstCard.getAttribute("data") == secondCard.getAttribute("data")) {
+		pairLeft -= 1;
+		resetBoard();
+		if (pairLeft == 0) {
+			gameOver();
+		}
+	} else {
+		gameCards.classList.add("locked");
+		setTimeout(function() { //unflip after a delay
+			firstCard.classList.remove("flipped");
+			secondCard.classList.remove("flipped");
+			resetBoard();
+		}, 1000);
+	}
+}
+
+function resetBoard() {
+	gameCards.classList.remove("locked");
+	[firstCard, secondCard] = [null, null];
+}
+
+function gameOver() {
+	setTimeout(function() {
+		gameInit()
+	}, 1000);
+}
 
 //creating events when clicking cards
 cards.forEach(card => card.addEventListener("click", function() {
-	card.classList.add("flipped");
-	setTimeout(function() { //unflip after a delay
-		card.classList.remove("flipped");
-	}, 2000);
+	flipCard(card);	
 }, false));
+
+//controls events
+document.getElementById("replay").addEventListener("click", gameOver);
+
+window.onload = gameInit;
